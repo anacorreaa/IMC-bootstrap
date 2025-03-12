1c0dba6e9c734b40f6269fe0db5f173f0c28f63c
Aluno <aluno@fatecitu.local>
2025-03-12 17:31:38 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,37 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
-  title = 'IMC';
+  peso!: number;
+  altura!: number;
+  imc!: number;
+  classificacao!: string;
+  imagem!: string;
+  title: any;
+
+  calcularIMC() {
+    if (this.peso && this.altura) {
+      this.imc = this.peso / (this.altura * this.altura);
+      this.definirClassificacao();
+    }
+  }
+
+  definirClassificacao() {
+    if (this.imc < 18.5) {
+      this.classificacao = 'Abaixo do peso';
+      this.imagem = 'https://example.com/baixo-peso.png';
+    } else if (this.imc < 24.9) {
+      this.classificacao = 'Peso normal';
+      this.imagem = 'https://example.com/peso-normal.png';
+    } else if (this.imc < 29.9) {
+      this.classificacao = 'Sobrepeso';
+      this.imagem = 'https://example.com/sobrepeso.png';
+    } else {
+      this.classificacao = 'Obesidade';
+      this.imagem = 'https://example.com/obesidade.png';
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
